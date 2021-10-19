b70d7c6a6dc64133963a3d7bb935595c6d27fc23
Stejskal Mirek <Stejskal.Mirek@stud.vassboskovice.cz>
2021-10-19 11:38:28 +0200

    Add project files.

---
+++ bmi 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace bmi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            string b;
+            double c;
+            
+            
+            Console.Clear();
+            Console.WriteLine("Kalkulačka BMI");
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("Zadej výšku(cm)");
+            a = Console.ReadLine();
+            int vyska = int.Parse(a);
+
+            Console.WriteLine("Zadej hmotnost (Kg) : ");
+            b = Console.ReadLine();
+            int hmotnost = int.Parse(b);
+
+            c = (vyska / 100) * (vyska / 100);
+            
+
+            Console.WriteLine("Tvoje BMI je " + c);
+            Console.ReadLine();
+            
+
+
+            
+
+
+
+
+            
+
+
+
+
+
+
+            
+
+
+
+        }
+    }
+}
